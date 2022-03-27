3663c32642806907baf6832e60088b098b8c2f52
marinacn19 <marinacuervanadal@outlook.es>
2022-03-27 11:01:12 +0200

    create some tables

--- SQLite.py
+++ SQLite.py
@@ -1,5 +1,8 @@
 import sqlite3
 
+import json
+
+
 def sql_update(con):
     cursorObj = con.cursor()
     cursorObj.execute('UPDATE usuarios SET nombre = "Sergio" where dni = "X"')
@@ -21,14 +24,21 @@ def sql_delete(con):
 def sql_delete_table(con):
     cursorObj = con.cursor()
     cursorObj.execute('drop table if exists usuarios')
+    cursorObj.execute('drop table if exists emails')
+    cursorObj.execute('drop table if exists legal')
+
     con.commit()
 
 def sql_create_tables(con):
     cursorObj = con.cursor()
-    cursorObj.execute("CREATE TABLE IF NOT EXISTS usuarios (telefono real, contrasena text, provincia text, permisos real, emails text, fechas text, ips text)")
-    cursorObj.execute("CREATE TABLE IF NOT EXISTS legal (cookies real, aviso real, proteccion_de_datos real, creacion real)")
+    cursorObj.execute("CREATE TABLE IF NOT EXISTS emails (usuario text primary key, total integer, phising integer, cliclados integer, FOREIGN KEY ('usuario') REFERENCES usuarios (nombre) )")
+    cursorObj.execute("CREATE TABLE IF NOT EXISTS usuarios (nombre text primary key, telefono integer, contrasena text, provincia text, permisos boolean, email text, fechas text, ips text)")
+    cursorObj.execute("CREATE TABLE IF NOT EXISTS legal (url text primary key, cookies real, aviso real, proteccion_de_datos real, creacion real)")
+    #cursorObj.execute("CREATE TABLE IF NOT EXISTS fechas ()
     #cursorObj.execute("INSERT INTO usuarios VALUES ('X', 'isaac', '1.85') ")
+    #sql_delete_table(con)
     con.commit()
+
 
 
 
