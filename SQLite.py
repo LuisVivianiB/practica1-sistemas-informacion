51f9587df94ad5eb0aaaf4319eea4bbb76c8b923
LuisVivianiB <81012836+LuisVivianiB@users.noreply.github.com>
2022-03-27 19:21:04 +0200

    insert data
    Co-Authored-By: marinacn19 <102314568+marinacn19@users.noreply.github.com>
    Co-Authored-By: iratxeloyo <102314788+iratxeloyo@users.noreply.github.com>

--- SQLite.py
+++ SQLite.py
@@ -5,8 +5,7 @@ import json
 
 
 
-def rellenartablas(con):
-
+def rellenarTablas(con):
     #abrir json
     with open("users.json", "r") as f:
         users_data = json.load(f)
@@ -19,9 +18,24 @@ def rellenartablas(con):
         for url in legal_data['legal'][i].keys():
             cursorObj.execute("INSERT INTO legal VALUES (?, ?, ?, ?, ?)", (url, legal_data['legal'][i][url]['cookies'], legal_data['legal'][i][url]['aviso'],legal_data['legal'][i][url]['proteccion_de_datos'], legal_data['legal'][i][url]['creacion']))
             con.commit()
+    k = 0
+    l = 0
+    for i in range(len(users_data['usuarios'])):
+        for nombre in users_data['usuarios'][i].keys():
+            cursorObj.execute("INSERT INTO emails VALUES (?, ?, ?, ?)", (nombre, users_data['usuarios'][i][nombre]['emails']['total'], users_data['usuarios'][i][nombre]['emails']['phishing'], users_data['usuarios'][i][nombre]['emails']['cliclados']))
 
+            for j in range(len(users_data['usuarios'][i][nombre]['ips'])):
+                cursorObj.execute("INSERT INTO IpsDeUsuarios VALUES (?, ?, ?)", (k, users_data['usuarios'][i][nombre]['ips'][j], i))
+                cursorObj.execute("INSERT INTO ips VALUES (?, ?)",(k, users_data['usuarios'][i][nombre]['ips'][j]))
 
-    ##user_data['usuarios'][i]['nombre']
+                k += 1
+
+            for j in range(len(users_data['usuarios'][i][nombre]['fechas'])):
+                cursorObj.execute("INSERT INTO fechas VALUES (?, ?)",(l, users_data['usuarios'][i][nombre]['fechas'][j]))
+                cursorObj.execute("INSERT INTO FechasDeUsuarios VALUES (?, ?, ?)", (l, users_data['usuarios'][i][nombre]['fechas'][j], i))
+                l += 1
+            cursorObj.execute("INSERT INTO usuarios VALUES (?, ?, ?, ?, ?, ?, ?, ?)", (nombre, users_data['usuarios'][i][nombre]['telefono'], users_data['usuarios'][i][nombre]['contrasena'], users_data['usuarios'][i][nombre]['provincia'], users_data['usuarios'][i][nombre]['permisos'], nombre, i, i))
+            con.commit()
 
 
 
@@ -58,17 +72,15 @@ def sql_delete_table(con):
 
 def sql_create_tables(con):
     cursorObj = con.cursor()
-    cursorObj.execute("CREATE TABLE IF NOT EXISTS usuarios (nombre text primary key, telefono integer, contrasena text, provincia text, permisos boolean, emails, fechas text, ips text, FOREIGN KEY ('emails') references emails(usuario))")
+    cursorObj.execute("CREATE TABLE IF NOT EXISTS usuarios (nombre text primary key, telefono integer, contrasena text, provincia text, permisos boolean, emails, fechas integer, ips integer, FOREIGN KEY ('emails') references emails(usuario))")
     cursorObj.execute("CREATE TABLE IF NOT EXISTS emails (usuario text primary key, total integer, phising integer, cliclados integer, FOREIGN KEY ('usuario') REFERENCES usuarios (nombre) )")
 
     cursorObj.execute("CREATE TABLE IF NOT EXISTS legal (url text primary key, cookies integer, aviso integer, proteccion_de_datos integer, creacion integer)")
     cursorObj.execute("CREATE TABLE IF NOT EXISTS fechas (idfechas integer primary key autoincrement, fecha text)")
-    cursorObj.execute("CREATE TABLE IF NOT EXISTS FechasDeUsuarios (idfechas integer primary key autoincrement, fechasdeusua text, usuario text, FOREIGN KEY ('usuario') references usuarios(nombre), FOREIGN KEY ('fechasdeusua') references fechas(fecha))")
+    cursorObj.execute("CREATE TABLE IF NOT EXISTS FechasDeUsuarios (idfechas integer primary key autoincrement, fechasdeusua text, usuario integer, FOREIGN KEY ('usuario') references usuarios(fechas), FOREIGN KEY ('fechasdeusua') references fechas(fecha))")
     cursorObj.execute("CREATE TABLE IF NOT EXISTS ips(idips integer primary key autoincrement, ip text)")
-    cursorObj.execute("CREATE TABLE IF NOT EXISTS IpsDeUsuarios ( idips integer primary key autoincrement, ipdeusua text, usuario text, FOREIGN KEY ('usuario') references usuarios(nombre), FOREIGN KEY ('ipdeusua') REFERENCES usuarios (nombre))")
-    #cursorObj.execute("INSERT INTO legal VALUES ('X', '4', '1', '3', '5')")
-    cursorObj.execute("INSERT INTO emails VALUES ('Luis', '1' ,'2','3')")
-    cursorObj.execute("INSERT INTO usuarios VALUES('Luis', '23243', 'luis123', 'Madrid', '0', 'Luis', 'fecha', 'ips')")
+    cursorObj.execute("CREATE TABLE IF NOT EXISTS IpsDeUsuarios ( idips integer primary key autoincrement, ipdeusua text, usuario integer, FOREIGN KEY ('usuario') references usuarios(ips), FOREIGN KEY ('ipdeusua') REFERENCES ips (ip))")
+
 
     #sql_delete_table(con)
 
@@ -78,7 +90,7 @@ def sql_create_tables(con):
 
 con = sqlite3.connect('database.db')
 sql_create_tables(con)
-rellenartablas(con)
+rellenarTablas(con)
 #sql_fetch(con)
 #sql_update(con)
 #sql_fetch(con)
