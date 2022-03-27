36308f1b7fcfe4381d6a64c09bf14d1890573a5c
marinacn19 <marinacuervanadal@outlook.es>
2022-03-27 12:37:30 +0200

    more tables

--- SQLite.py
+++ SQLite.py
@@ -3,6 +3,20 @@ import sqlite3
 import json
 
 
+
+
+def rellenartablas(con):
+
+    #abrir json
+    with open("users.json", "r") as f:
+        users_data = json.load(f)
+
+    with open("legal.json", "r") as f1:
+        legal_data = json.load(f1)
+
+
+
+
 def sql_update(con):
     cursorObj = con.cursor()
     cursorObj.execute('UPDATE usuarios SET nombre = "Sergio" where dni = "X"')
@@ -26,19 +40,27 @@ def sql_delete_table(con):
     cursorObj.execute('drop table if exists usuarios')
     cursorObj.execute('drop table if exists emails')
     cursorObj.execute('drop table if exists legal')
-
+    cursorObj.execute('drop table if exists fechas')
+    cursorObj.execute('drop table if exists FechasDeUsuarios')
+    cursorObj.execute('drop table if exists ips')
+    cursorObj.execute('drop table if exists IpsDeUsuarios')
     con.commit()
+
 
 def sql_create_tables(con):
     cursorObj = con.cursor()
-    cursorObj.execute("CREATE TABLE IF NOT EXISTS emails (usuario text primary key, total integer, phising integer, cliclados integer, FOREIGN KEY ('usuario') REFERENCES usuarios (nombre) )")
     cursorObj.execute("CREATE TABLE IF NOT EXISTS usuarios (nombre text primary key, telefono integer, contrasena text, provincia text, permisos boolean, email text, fechas text, ips text)")
-    cursorObj.execute("CREATE TABLE IF NOT EXISTS legal (url text primary key, cookies real, aviso real, proteccion_de_datos real, creacion real)")
-    #cursorObj.execute("CREATE TABLE IF NOT EXISTS fechas ()
-    #cursorObj.execute("INSERT INTO usuarios VALUES ('X', 'isaac', '1.85') ")
-    #sql_delete_table(con)
-    con.commit()
+    cursorObj.execute("CREATE TABLE IF NOT EXISTS emails (usuario text primary key, total integer, phising integer, cliclados integer, FOREIGN KEY ('usuario') REFERENCES usuarios (nombre) )")
 
+    cursorObj.execute("CREATE TABLE IF NOT EXISTS legal (url text primary key, cookies real, aviso real, proteccion_de_datos real, creacion real)")
+    cursorObj.execute("CREATE TABLE IF NOT EXISTS fechas (idfechas integer primary key autoincrement, fecha text)")
+    cursorObj.execute("CREATE TABLE IF NOT EXISTS FechasDeUsuarios (idfechas integer primary key autoincrement, fechasdeusua text, usuario text, FOREIGN KEY ('usuario') references usuarios(nombre), FOREIGN KEY ('fechasdeusua') references fechas(fecha))")
+    cursorObj.execute("CREATE TABLE IF NOT EXISTS ips(idips integer primary key autoincrement, ip text)")
+    cursorObj.execute("CREATE TABLE IF NOT EXISTS IpsDeUsuarios ( idips integer primary key autoincrement, ipdeusua text, usuario text, FOREIGN KEY ('usuario') references usuarios(nombre), FOREIGN KEY ('ipdeusua') REFERENCES usuarios (nombre))")
+    cursorObj.execute("INSERT INTO legal VALUES ('X', '4', '1', '3', '5')")
+    #sql_delete_table(con)
+
+    con.commit()
 
 
 
